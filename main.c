5f9ff9d24b46f6a952809d804d529f7b55d778f9
Neal <neal@ineal.me>
2011-12-05 19:03:37 -0600

    didn't mean to comment that :p

--- main.c
+++ main.c
@@ -61,7 +61,7 @@ int main(int argc, CmdArg* argv) {
 	char result[0x10];
 	if(!gGpHasInit || gCmdCount == 0) {
 		if(gp_init()) {
-//			puts("Unable to initialize greenpois0n!!\n");
+			puts("Unable to initialize greenpois0n!!\n");
 			return -1;
 		}
 //		puts("Greenpois0n initialized\n");
